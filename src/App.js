eb69b9c8eaa020e21efece0c079be4b8be8b423a
unknown <maitrish03@gmail.com>
2022-03-08 19:22:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import GeneralInfo from './Components/GeneralInfo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+          name:'',
+      }
+    }
 
-export default App;
+  
+  handleChange=(e)=>{
+    const newValue=e.target.value
+    this.setState({
+        name:newValue
+    })
+  }
+  handleSubmit=(e)=>{
+    e.preventDefault()
+  }
+    
+  render() {
+    return (
+      <>
+      <div>
+        
+          <input onChange={this.handleChange}  id='name' type='text' placeholder='Enter full name' />
+          <input onSubmit={this.handleSubmit} type='submit'/>
+          <GeneralInfo  data={this.sendDataToChild}/>
+        
+      </div>
+      
+      </>
+      
+      
+    )
+  }
+  }
+  // <input id='email' type='email' placeholder='Enter Email' />
+          
+  // <input id='phone' type='text' placeholder='Enter Phone number' />
+  // <input id='location' type='text' placeholder='Enter Location' />
+  // <input id='gitHub' type='text' placeholder='Enter Github' />
---
+++ src/Components/GeneralInfo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class GeneralInfo extends Component {
+   
+  render() {
+    return (
+      <div>{this.props.data}</div>
+    )
+  }
+}
+
